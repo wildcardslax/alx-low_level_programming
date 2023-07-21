31656cc1f8b3e40465319135c474648a0921c4a2
Atlaz <wildcardslax>
2023-07-21 05:27:32 -0700

    0x03. C - Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 int main(void)
 {
-    int i;
-    printf("Infinite loop incoming :(\n");
-    i = 0;
-    while (1) // This loop will run indefinitely since the condition is always true (1).
-    {
-        putchar(i);
-        i++; // Increment the value of i to prevent an infinite loop that prints the same character repeatedly.
-    }
-    printf("Infinite loop avoided! \\o/\n");
-    return (0);
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
